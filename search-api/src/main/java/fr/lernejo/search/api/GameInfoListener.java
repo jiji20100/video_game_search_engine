f679b8791f0dc2ea4fdac31b492e14a8c09e6fd1
jiji20100 <jihadrifi909@gmail.com>
2023-12-21 23:07:45 +0100

    correct listener

--- search-api/src/main/java/fr/lernejo/search/api/GameInfoListener.java
+++ search-api/src/main/java/fr/lernejo/search/api/GameInfoListener.java
@@ -1,24 +1,23 @@
 package fr.lernejo.search.api;
 
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.xcontent.XContentType;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Map;
 
 @Component
 public class GameInfoListener {
 
-    private final RestHighLevelClient restHighLevelClient;
+    private final RestHighLevelClient client;
 
-    @Autowired
-    public GameInfoListener(RestHighLevelClient restHighLevelClient) {
-        this.restHighLevelClient = restHighLevelClient;
+    public GameInfoListener(RestHighLevelClient client) {
+        this.client = client;
     }
 
     @RabbitListener(queues = AmqpConfiguration.GAME_INFO_QUEUE)
@@ -26,9 +25,11 @@ public class GameInfoListener {
         try {
             IndexRequest indexRequest = new IndexRequest("games")
                 .id(gameId)
-                .source(message);
-            restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+                .source(message, XContentType.JSON);
 
+            IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+
+            System.out.println("Document indexed: " + indexResponse);
         } catch (IOException e) {
             System.err.println("Error while indexing game " + gameId + ": " + e.getMessage());
         }
