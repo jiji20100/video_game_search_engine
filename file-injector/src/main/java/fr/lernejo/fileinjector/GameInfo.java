b5ac76bad551bdaf436dd0835f5e4db2839a3a76
jiji20100 <jihadrifi909@gmail.com>
2023-12-21 23:08:31 +0100

    adding file injector and record GameInfo

---
+++ file-injector/src/main/java/fr/lernejo/fileinjector/GameInfo.java
@@ -0,0 +1,18 @@
+package fr.lernejo.fileinjector;
+
+import java.io.Serializable;
+
+public record GameInfo (
+    int id,
+    String title,
+    String thumbnail,
+    String short_description,
+    String game_url,
+    String genre,
+    String platform,
+    String publisher,
+    String developer,
+    String release_date,
+    String freetogame_profile_url
+) implements Serializable
+{ }
--- file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
@@ -1,16 +1,36 @@
 package fr.lernejo.fileinjector;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+
+import java.io.File;
+import java.util.List;
 
 @SpringBootApplication
 public class Launcher {
 
     public static void main(String[] args) {
         try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
+            RabbitTemplate rabbitTemplate = springContext.getBean(RabbitTemplate.class);
+            rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
 
-            System.out.println("Hello after starting Spring");
-        }
+            ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+            CollectionType collectionType = objectMapper.getTypeFactory().constructCollectionType(List.class, GameInfo.class);
+            List<GameInfo> messages = objectMapper.readValue(new File(args[0]), collectionType);
+            for (GameInfo message : messages) {
+                rabbitTemplate.convertAndSend("game_info", message, m -> {
+                    m.getMessageProperties().setHeader("game_id", message.id());
+                    m.getMessageProperties().setContentType("application/json");
+                    return m;
+                });
+            }
+        } catch (Exception e) { System.exit(2); }
     }
 }
