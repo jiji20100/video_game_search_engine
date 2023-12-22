56ae968789524ce2d399df311f0d7b7787e2c7c6
jiji20100 <jihadrifi909@gmail.com>
2023-12-22 19:43:36 +0100

    adding searchApi test

---
+++ search-api/src/test/java/fr/lernejo/search/api/LauncherTest.java
@@ -0,0 +1,9 @@
+package fr.lernejo.search.api;
+import org.junit.jupiter.api.Test;
+public class LauncherTest {
+    @Test
+    void launcher_is_ok()
+    {
+        Launcher.main(new String[]{});
+    }
+}
